2d328829d919cf02fdf3c1abcf3834f7b5ef7664
Alkur <vualkurs@gmail.com>
2022-02-13 19:35:40 +0600

    initial version

---
+++ README.md
@@ -0,0 +1,12 @@
+get data from: https://www.kaggle.com/koryakinp/fingers
+tree:
+    .
+    main.py
+    images/
+        train/
+            data.npz
+            *.png
+        test/
+            data.npz
+            *.png
+    etc.
---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from tensorflow import keras
+
+def process_frame(frame):
+    return cv2.cvtColor(cv2.resize(frame, (128, 128)), cv2.COLOR_BGR2GRAY)
+
+def _main():
+    model_dir = os.path.join('models', 'dummy-model')
+    model = keras.models.load_model(model_dir)
+    src = cv2.VideoCapture(0)
+    win_name = 'preview'
+    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    while cv2.waitKey(1) != 27:
+        has_frame, frame = src.read()
+        if not has_frame:
+            break
+
+        pframe = process_frame(frame)
+        label = model.predict(np.expand_dims(pframe, axis=0)).argmax()
+        text = "Prediction: " + str(label)
+        cv2.putText(frame, text, (50, 50),
+                fontFace=cv2.FONT_HERSHEY_PLAIN,
+                fontScale=2.3,
+                color=(255, 0, 0),
+                thickness=2,
+                lineType=cv2.LINE_AA,
+        )
+        cv2.imshow(win_name, frame)
+    src.release()
+    cv2.destroyWindow(win_name)
+
+
+if __name__ == '__main__':
+    _main()
