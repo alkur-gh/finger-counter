a342c7791a932ef99c7ae78cfb97a6d5badef17d
Alkur <vualkurs@gmail.com>
2022-03-03 22:16:47 +0600

    back2 bgsub, still shit, training dataset is shit Copege

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from tensorflow import keras
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import preprocess_input
 
 
 def attach_text(image, text):
@@ -20,44 +17,73 @@ def attach_text(image, text):
     )
 
 
+def preprocess_frame(frame, bgframes):
+    bgsub = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
+    for bgframe in bgframes:
+        bgsub.apply(bgframe)
+    mask = bgsub.apply(frame)
+    #return np.expand_dims(cv2.cvtColor(cv2.bitwise_and(frame, frame, mask=mask), cv2.COLOR_BGR2GRAY), axis=2)
+    return np.expand_dims(mask, axis=2)
+
+
 def _main():
-    width, height = 300, 300
+    width, height = 128, 128
     src = cv2.VideoCapture(0 if len(sys.argv) == 1 else sys.argv[1])
     src.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     src.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     win_name = 'preview'
     cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
+    model = keras.models.load_model('bgmodel')
+
     default_text_params = {
             'fontFace': cv2.FONT_HERSHEY_PLAIN,
-            'fontScale': 1,
+            'fontScale': 0.7,
             'color': (255, 0, 0),
             'thickness': 1,
             'lineType': cv2.LINE_AA,
     }
-
-    base_model = VGG16(weights='imagenet', include_top=False)
-    model = keras.models.Model(inputs=base_model.input, outputs=base_model.get_layer('block2_conv2').output)
-
+   
+    frame_rate = 7
+    bgframes = []
     s_time = time.time()
-    while cv2.waitKey(1) != 27:
+    key = None
+    nframe = 0
+    while True:
+        nframe += 1
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+        if nframe == 1 or key == 32:
+            bgframes = []
+            for _ in range(30):
+                bgframes.append(cv2.resize(src.read()[1], (width, height)))
+
         c_time = time.time()
         elapsed = c_time - s_time
         has_frame, frame = src.read()
         if not has_frame:
             break
+        if elapsed < 1. / frame_rate:
+            continue
         fps = int(1.0 / elapsed)
         s_time = c_time
-
+        
         strings = []
 
         strings.append(f'FPS: {fps}')
         
-        x = preprocess_input(np.expand_dims(frame, axis=0))
-        features = model.predict(x)
-        print(features.shape)
+        frame = cv2.resize(frame, (width, height))
+        frame = preprocess_frame(frame, bgframes)
+        
+        ypred = model.predict(np.expand_dims(frame, axis=0))[0]
+        label = ypred.argmax()
+        conf = np.exp(ypred[label])/np.exp(ypred).sum()
+        print(np.exp(ypred)/np.exp(ypred).sum())
 
-        cv2.putText(frame, ' | '.join(strings), (20, 20), **default_text_params)
+        strings.append(f'{ypred.argmax()} {conf:.2}')
+
+        cv2.putText(frame, ' | '.join(strings), (10, 10), **default_text_params)
         cv2.imshow(win_name, frame)
 
     src.release()
